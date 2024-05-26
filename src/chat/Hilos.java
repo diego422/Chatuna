e182aa2590584ee7034609f87105fd5743c3a83c
diego422 <diegoherreralopez9@gmail.com>
2024-05-26 15:30:00 -0600

    Se modifico las clases
    Se removieron comentarios

--- src/chat/Hilos.java
+++ src/chat/Hilos.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package chat;
 
 /**
  *
- * @author pablonoguera
+ * @author 
  */
 
 public class Hilos extends Thread {
